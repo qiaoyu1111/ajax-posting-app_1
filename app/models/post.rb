f740920534f01a093c97becdfa7ce660389cdc64
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-12 22:32:15 +0800

    add

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,14 @@ class Post < ApplicationRecord
   end
 
   belongs_to :category, :optional => true
+
+  has_many :scores, :class_name => "PostScore"
+
+  def find_score(user)
+    user && self.scores.where( :user_id => user.id ).first
+  end
+
+  def average_score
+    self.scores.average(:score)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812135558) do
+ActiveRecord::Schema.define(version: 20170812141614) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20170812135558) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_loves_on_post_id"
     t.index ["user_id"], name: "index_loves_on_user_id"
+  end
+
+  create_table "post_scores", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "score"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_scores_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
