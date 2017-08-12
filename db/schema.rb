5761a47aa3be2623494870f3f049780fa5f1e139
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-12 21:43:15 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812114422) do
+ActiveRecord::Schema.define(version: 20170812133737) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -21,11 +21,21 @@ ActiveRecord::Schema.define(version: 20170812114422) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "loves", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_loves_on_post_id"
+    t.index ["user_id"], name: "index_loves_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "flag_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170812114422) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
