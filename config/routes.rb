b6bfb4828e69522377d08b1df5295aa85746f818
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-12 14:01:58 +0800

    add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
+  
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
   get "/jquery-3" => "pages#jquery_3"
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
 
-  root "pages#jquery_1"
+  # root "pages#jquery_1"
+  root "posts#index"
 end
