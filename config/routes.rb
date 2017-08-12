ffe2a093dc7341b8491c3e116ef6c0c85c21dfa2
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-12 20:34:05 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+    end
+  end
   devise_for :users
-  
+
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
   get "/jquery-3" => "pages#jquery_3"
